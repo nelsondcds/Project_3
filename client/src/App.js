64c67ce18fed82276b644e9af939bdb9d2955039
rldyd <kyleahn89@gmail.com>
2021-10-21 00:09:56 -0400

    re-create register & login without jwt in server side

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
-import { Container } from "react-bootstrap";
-import Layout from "./layouts/Layout";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+import { AuthProvider } from "./context/auth";
+import AuthRoutes from "./util/AuthRoute";
+
+import MenuBar from "./components/MenuBar";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <Layout>
-      <Container style={{ minHeight: "85vh" }}>App</Container>
-    </Layout>
+    <AuthProvider>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" pages={Home} />
+          <Route exact path="/login" pages={Login} />
+          <Route exact path="/register" pages={Register} />
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
