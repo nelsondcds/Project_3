128516053411483635e791ea5a1dd960ebaf857e
Maxdamoe <max.simpson@gmail.com>
2021-10-24 12:54:47 -0500

    user model changes

--- server/models/user.js
+++ server/models/user.js
@@ -1,40 +1,10 @@
 const { model, Schema } = require("mongoose");
-const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-  }, 
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true, 
-  }
+  username: String,
+  password: String,
+  email: String,
+  createdAt: String,
 });
 
-userSchema.pre('save', async function(next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
-
-  next();
-});
-
-// compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function(password) {
-  return bcrypt.compare(password, this.password);
-};
-
-userSchema.virtual('friendCount').get(function() {
-  return this.friends.length;
-});
-
-
-const User = model("User", userSchema);
-
-module.exports = User;
+module.exports = model("User", userSchema);
