1985e7d7e79d371fe2821006443c156ca006b2f7
rldyd <kyleahn89@gmail.com>
2021-10-25 19:26:58 -0400

    before pulling-2

--- server/models/user.js
+++ server/models/user.js
@@ -2,34 +2,33 @@ const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
- {
-   username: {
-     type: String,
-     required: true,
-     unique: true,
-   },
-   email: {
-     type: String,
-     required: true,
-     unique: true,
-     match: [/.+@.+\..+/, "Must use a valid email address"],
-   },
-   password: {
-     type: String,
-     required: true,
-   },
- },
- 
- {
-   toJSON: {
-     virtuals: true,
-   },
- }
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must use a valid email address"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
 );
 
-
 userSchema.pre("save", async function (next) {
- if (this.isNew || this.isModified("password")) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -38,11 +37,9 @@ userSchema.pre("save", async function (next) {
 });
 
 // compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function(password) {
+userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
-
-
 
 const User = model("User", userSchema);
 
